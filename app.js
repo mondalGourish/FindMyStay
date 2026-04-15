6e6058d5ba7f22563166d83904c07b7217148147
mondalGourish <gourishmondal02@gmail.com>
2026-04-15 10:50:45 +0530

    some minor chnages

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 const store = MongoStore.create({
     mongoUrl : dbUrl,
     crypto:{
-        secret:"mysupersecretcode",
+        secret: process.env.SECRET,
     },
     touchAfter: 24*3600,
 });
@@ -67,7 +67,7 @@ store.on("error",()=>{
 //session adjustments
 const sessionOptions = {
     store,
-    secret:"mysupersecretcode",
+    secret: process.env.SECRET,
     resave:false,
     saveUninitialized:true,
     //expiry date for the cookie i.e set to the date after 1 week
